bcae0798ed50198849cdd98aac60b56849bc35ea
Omar El-Sharoud <f.elsharoud@gmail.com>
2025-09-09 18:53:21 +0100

    portoflio

--- .idea/app.py
+++ .idea/app.py
@@ -359,3 +359,6 @@ if __name__ == '__main__':
 
 from Flask_Portfolio_Omar_Elsharoud import app as application
 
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
